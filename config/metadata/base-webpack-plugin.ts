1093694afb3e44983c4056e0d85f5b05f88dfa7a
kPherox <admin@mail.kr-kp.com>
2021-05-04 02:58:54 +0900

    build valid userscript header

--- config/metadata/base-webpack-plugin.ts
+++ config/metadata/base-webpack-plugin.ts
@@ -1,4 +1,5 @@
 
+import {URL} from 'url';
 import {
   ModuleFilenameHelpers,
   Compilation,
@@ -6,13 +7,13 @@ import {
   WebpackPluginInstance,
   sources,
 } from 'webpack';
-import {UserScriptMetadata} from './types';
+import {UserScriptMetadata, ValueOf} from './types';
 
 /**
  * Prepend UserScript Metadata to `*.user.js` files
  */
 class UserScriptMetadataPlugin implements WebpackPluginInstance {
-  headers: {[key: string]: string}
+  metadataList: {[key: string]: UserScriptMetadata}
 
   /**
    * Initialize UserScript metadata plugin
@@ -20,30 +21,159 @@ class UserScriptMetadataPlugin implements WebpackPluginInstance {
    */
   constructor(options: {[key: string]:UserScriptMetadata}) {
     const entryPoints = Object.keys(options);
-    this.headers = {};
+    this.metadataList = {};
     for (const name of entryPoints) {
       const metadata = {name, ...options[name]};
-      this.headers[name] = this._buildHeader(metadata);
+      this.metadataList[name] = metadata;
     }
   }
 
   /**
-   * Build UserScript header from UserScriptMetadata object.
-   * @param {UserScriptMetadata} metadata metadata struct.
-   * @return {String} userscript metadata key-value.
+   * Transform to string from ValueOf<Metadata>
+   * @param {ValueOf<UserScriptMetadata>} value metadata value.
+   * @return {String | undefined} kebabCase/colon separated string.
    */
-  _buildHeader(metadata: UserScriptMetadata): string {
+  _metadataToString(value: ValueOf<UserScriptMetadata>): string[] | undefined {
+    if (value == null) {
+      return undefined;
+    }
+    if (!Array.isArray(value)) {
+      if (typeof value === 'number') {
+        return [String(value)];
+      }
+      if (typeof value === 'string') {
+        return [value];
+      }
+      if (value instanceof URL) {
+        return [value.toString()];
+      }
+      if ('year' in value) {
+        let copyright = `(c) ${value.year} ${value.name}`;
+        copyright = value.email ? `${copyright} <${value.email}>` : copyright;
+        copyright = value.url ? `${copyright} (${value.url})` : copyright;
+        return [copyright];
+      }
+      if ('name' in value) {
+        let name = value.name;
+        name = value.email ? `${name} <${value.email}>` : name;
+        name = value.url ? `${name} (${value.url})` : name;
+        return [name];
+      }
+      return Object.entries(value).map(([key, value]) => {
+        return `${key} ${value}`;
+      });
+    }
+    let result: string[] | undefined = undefined;
+    for (const v of value) {
+      const vs = this._metadataToString(v);
+      if (vs == null) {
+        continue;
+      }
+      result = [
+        ...result || [],
+        ...vs,
+      ];
+    }
+    return result;
+  }
+
+  /**
+   * Build UserScript header from UserScriptMetadata object.
+   * @param {[string, string?][]} metadata metadata entries.
+   * @param {number} length metadata key length.
+   * @return {String} userscript metadata headers.
+   */
+  _buildHeader(metadata: [string, string?][], length: number): string {
     const result = ['// ==UserScript=='];
-    type Key = keyof typeof metadata;
-    const entryPoints = Object.keys(metadata);
-    const long = entryPoints.reduce(
-        (res, name) => name.length > res ? name.length : res,
-        0);
-    for (const name of entryPoints) {
-      result.push(`// @${name.padEnd(long+1, ' ')}${metadata[name as Key]}`);
+    for (const [key, value] of metadata) {
+      if (value == null) {
+        result.push(`// @${key}`);
+        continue;
+      }
+      result.push(`// @${key.padEnd(length+3, ' ')}${value}`);
     }
     result.push('// ==/UserScript==');
     return result.join('\n');
+  }
+
+  /**
+   * UserScript headers.
+   * @return {Object.<string, string>} userscript metadata headers.
+   */
+  get headers(): {[key: string]: string} {
+    const res: {[key: string]: string} = {};
+    for (const [name, metadata] of Object.entries(this.metadataList)) {
+      let metadataEntries = Object.entries(metadata);
+      metadataEntries = metadataEntries.map(([key, value]) => {
+        switch (key) {
+          case 'runAt':
+            key = 'run-at';
+            break;
+          case 'usoScript':
+            key = 'uso:script';
+            break;
+          case 'usoVersion':
+            key = 'uso:version';
+            break;
+          case 'usoTimestamp':
+            key = 'uso:timestamp';
+            break;
+          case 'usoHash':
+            key = 'usoHash';
+            break;
+          case 'usoRating':
+            key = 'uso:rating';
+            break;
+          case 'usoInstalls':
+            key = 'uso:installs';
+            break;
+          case 'usoReviews':
+            key = 'uso:reviews';
+            break;
+          case 'usoDiscussions':
+            key = 'uso:discussions';
+            break;
+          case 'usoFans':
+            key = 'uso:fans';
+            break;
+          case 'usoUnlisted':
+            key = 'uso:unlisted';
+            break;
+          default:
+            break;
+        }
+        return [key, value];
+      });
+
+      const keyValues: [string, string?][] = [];
+      for (const [key, value] of metadataEntries) {
+        const values = this._metadataToString(value);
+        if (values == null &&
+            ['noframes', 'unwrap', 'usoUnlisted'].includes(key)) {
+          keyValues.push([key, undefined]);
+          continue;
+        }
+        if (values == null) {
+          continue;
+        }
+        for (const v of values) {
+          if (key === 'name') {
+            const separate = v.indexOf(' ');
+            keyValues.push([
+              `${key}:${v.slice(0, separate)}`,
+              v.slice(separate+1),
+            ]);
+          } else {
+            keyValues.push([key, v]);
+          }
+        }
+      }
+      const keyLength = keyValues.reduce((res, [key]) => {
+        return key.length > res ? key.length : res;
+      }, 0);
+      res[name] = this._buildHeader(keyValues, keyLength);
+    }
+    return res;
   }
 
   /**
