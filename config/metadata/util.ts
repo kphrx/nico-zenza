a47d22cdd1d9aabd52d2084685de0c657e93bed7
kPherox <admin@mail.kr-kp.com>
2021-05-27 19:57:38 +0900

    refactor: requireURLs function

--- config/metadata/util.ts
+++ config/metadata/util.ts
@@ -4,24 +4,36 @@ import packageMetadata from './package';
 export interface RequireExternals {
   [key: string]: string[] | string;
 }
+
 /**
- * Transform dependensies key-value to jsdelivr urls array
+ * Transform dependency name and path to jsdelivr url.
+ * @param {string} name dependency name.
+ * @param {string} version dependency version.
+ * @param {string} path jsdelivr path.
+ * @return {string[]} the metadata block as a string.
+ */
+function jsDeliver(name: string, version: string, path: string): string {
+  return `https://cdn.jsdelivr.net/npm/${name}@${version}${path}`;
+}
+
+/**
+ * Transform dependencies key-value to jsdelivr urls array
  * @param {RequireExternals} externals dependencies with jsdelivr path.
  * @return {string[]} the metadata block as a string.
  */
 export function requireURLs(externals: RequireExternals): string[] {
   return Object.entries(externals)
-      .reduce((res: string[], [name, path]) => {
-        const fn = (name: string, path: string) => {
-          const version = packageMetadata.dependencies?.[name];
-          if (version) {
-            res.push(`https://cdn.jsdelivr.net/npm/${name}@${version}${path}`);
-          }
-        };
-        if (Array.isArray(path)) {
-          path.forEach(fn.bind(undefined, name));
-        } else {
-          fn(name, path);
+      .reduce((res: string[], [name, paths]) => {
+        const version = packageMetadata.dependencies?.[name];
+        if (version == null) {
+          return res;
+        }
+        if (!Array.isArray(paths)) {
+          res.push(jsDeliver(name, version, paths));
+          return res;
+        }
+        for (const path of paths) {
+          res.push(jsDeliver(name, version, path));
         }
         return res;
       }, []);
