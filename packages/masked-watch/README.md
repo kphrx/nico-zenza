b1768c09fede5306bb5c630fa97388d55481b6d9
kPherox <admin@mail.kr-kp.com>
2022-03-19 03:53:04 +0900

    migrate to workspace: MaskedWatch

---
+++ packages/masked-watch/README.md
@@ -0,0 +1,15 @@
+# Masked Watch
+動画上のテキストや顔を検出してコメントを透過する
+
+
+## What is this?
+[segabito/ZenzaWatch] の `MaskedWatch.user.js` を TypeScript で書き直して生成するやつ
+
+
+## Installation
+TODO
+greasyfork.org に上げるか GitHub Pages にいい感じに出すか
+
+
+
+[segabito/ZenzaWatch]: https://github.com/segabito/ZenzaWatch
---
+++ packages/masked-watch/rollup.config.js
@@ -0,0 +1,37 @@
+/* eslint-env node */
+import typescript from '@rollup/plugin-typescript';
+import metablock from 'rollup-plugin-userscript-metablock';
+import pkg from './package.json';
+
+const production = process.env.NODE_ENV === 'production';
+
+let filename = process.env.npm_package_name.split('/').pop();
+let version = process.env.npm_package_version;
+
+if (!production) {
+  filename += '+dev';
+  version += `-dev.${new Date().getTime()}`;
+}
+
+export default {
+  input: 'src/main.ts',
+  output: {
+    file: `dist/${filename}.user.js`,
+    format: 'esm',
+    sourcemap: !production && 'inline',
+  },
+  plugins: [
+    typescript(),
+    metablock({
+      file: './metadata.yml',
+      override: {
+        version,
+        description: pkg.description,
+        license: pkg.license,
+        author: pkg.author,
+        source: pkg.repository.url,
+        homepage: pkg.homepage,
+      },
+    }),
+  ],
+};
