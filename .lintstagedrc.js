b8db2b067dbe6f3d90abb762094b5f1a7c917e0a
kPherox <admin@mail.kr-kp.com>
2022-04-07 21:30:39 +0900

    fix(lint-staged): tsc build and clean command

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,6 +1,8 @@
 export default {
-  "*.ts": () =>
-    "tsc -b tsconfig.build.json && tsc -b tsconfig.build.json --clean",
+  "*.ts": () => [
+    "tsc -b tsconfig.build.json",
+    "tsc -b tsconfig.build.json --clean",
+  ],
   "*.{ts,js}": ["eslint --fix", "prettier --write --ignore-unknown"],
   "!*.{ts,js}": "prettier --write --ignore-unknown",
 };
