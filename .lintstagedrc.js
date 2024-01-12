a73e9585672c62ffff2333aad848faa2a29e7651
kPherox <kphrx@kpherox.dev>
2024-01-12 16:45:42 +0900

    chore: fix prettier on lint-staged (#14)
    no check typescript

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,11 +1,5 @@
 export default {
-  "*.ts": () => [
-    "tsc --build tsconfig.build.json",
-    "tsc --build tsconfig.build.json --clean",
-    "eslint --ignore-path .gitignore --fix",
-    "prettier --ignore-path .gitignore --write --ignore-unknown",
-  ],
-  "*.js": [
+  "*.{ts,js}": [
     "eslint --ignore-path .gitignore --fix",
     "prettier --ignore-path .gitignore --write --ignore-unknown",
   ],
