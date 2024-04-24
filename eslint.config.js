0dec6a62d36871fdfd1378b2f83903bc17c9e373
kPherox <kphrx@kpherox.dev>
2024-04-24 15:15:16 +0900

    chore: update ignore for eslint

--- eslint.config.js
+++ eslint.config.js
@@ -6,6 +6,9 @@ import tseslint from "typescript-eslint";
 import prettier from "eslint-config-prettier";
 
 export default tseslint.config(
+  {
+    ignores: ["dist/", "**/.rollup.cache/"],
+  },
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
   {
